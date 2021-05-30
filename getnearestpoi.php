308a3e5000e1f76ab08509f0b736218ecd82d27c
To Chan <kaitockt@gmail.com>
2021-05-30 13:35:36 +0100

    Added place_id to POI API request

--- getnearestpoi.php
+++ getnearestpoi.php
@@ -5,9 +5,10 @@ $key = file_get_contents("apikey.txt", true);
 echo json_encode(getNearestPlace($_GET['latlng'], $_GET['type'], $key));
 
 function getNearestPlace($latlng, $input, $key) {
+    $fields = ["name", "geometry", "place_id"];
     $curl = curl_init();
     curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=$input&inputtype=textquery&fields=name,geometry&locationbias=point:$latlng&key=$key",
+        CURLOPT_URL => "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=$input&inputtype=textquery&fields=".implode(",", $fields)."&locationbias=point:$latlng&key=$key",
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_TIMEOUT => 30,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
@@ -22,10 +23,9 @@ function getNearestPlace($latlng, $input, $key) {
     
     curl_close($curl);
     $response = json_decode($response, true);
-    // var_dump($response);
-    // return false;
     $res = array(
         "name" => $response['candidates'][0]['name'],
+        "place_id" => $response['candidates'][0]['place_id'],
         "lat" => $response['candidates'][0]['geometry']['location']['lat'],
         "lng" => $response['candidates'][0]['geometry']['location']['lng'],
     );
