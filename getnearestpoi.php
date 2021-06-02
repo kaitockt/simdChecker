bb809238bd4e462e8cc22fac115d7b225d2fc3c5
To Chan <kaitockt@gmail.com>
2021-06-02 13:32:29 +0100

    Rewrote a api for nearest poi

--- getnearestpoi.php
+++ getnearestpoi.php
@@ -1,33 +1,16 @@
 <?php
+/**
+ * @param {string} origin "lat,lng"
+ * @param {string} type
+ * @return {string} time
+ */
+
 //get api key
 $key = file_get_contents("apikey.txt", true);
+$fp = new FindPlace();
+$dm = new DistanceMatrix();
 
-echo json_encode(getNearestPlace($_GET['latlng'], $_GET['type'], $key));
-
-function getNearestPlace($latlng, $input, $key) {
-    $fields = ["name", "geometry", "place_id"];
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=$input&inputtype=textquery&fields=".implode(",", $fields)."&locationbias=point:$latlng&key=$key",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "GET",
-        CURLOPT_HTTPHEADER => array(
-            "cache-control: no-cache"
-        ),
-    ));
-    
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    
-    curl_close($curl);
-    $response = json_decode($response, true);
-    $res = array(
-        "name" => $response['candidates'][0]['name'],
-        "place_id" => $response['candidates'][0]['place_id'],
-        "lat" => $response['candidates'][0]['geometry']['location']['lat'],
-        "lng" => $response['candidates'][0]['geometry']['location']['lng'],
-    );
-    return $res;
-}
+$fp->curl($_GET['origin'], $_GET['type']);
+$destination = $fp->getLat().",".$fp->getLng();
+$dm->curl($_GET['origin'], $destination);
+return $dm->getDuration();
